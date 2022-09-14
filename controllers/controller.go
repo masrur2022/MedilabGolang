7e938e7b298e459d94fb8098402dadfcaa6703f8
masrur <micketrust@gmail.com@gmail.com>
2022-09-14 19:39:16 +0500

    hello

--- controllers/controller.go
+++ controllers/controller.go
@@ -579,7 +579,7 @@ func Logout(c *gin.Context) {
 	})
 }
 func Cors(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "https://127.0.0.1:3000")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:3000")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, ResponseType, accept, origin, Cache-Control, X-Requested-With")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
